9b6b4493d0f943428f40cc263a33c431a3fd74ca
alexskrn <askrn123@gmail.com>
2024-01-13 18:56:30 +0200

    Fixed fieldAlreadyExists error

--- tmxtagger/tmx_tradosizer.py
+++ tmxtagger/tmx_tradosizer.py
@@ -87,19 +87,51 @@ def get_tmx_file_contents(file_path):
 def add_tree_elements(tmx_contents_string, prop_text):
     """Return a modified ElementTree parsed from the tmx file content."""
     root = ET.fromstring(tmx_contents_string)
-    # Modify the <header> element by adding attributes & sub-elements
-    for header in root.findall('header'):
-        header.set('creationtool', "SDL Language Platform")
-        header.set('o-tmf', "SDL TM8 Format")
+
+    header = root.find('header')
+    header.set('creationtool', "SDL Language Platform")
+    header.set('o-tmf', "SDL TM8 Format")
+
+    header_attib_list = [subel.attrib for subel in header.iter()]
+    header_attib_prop_values = []
+    for d in header_attib_list:
+        header_attib_prop_values.extend(list(d.values()))
+    if 'x-Recognizers' not in header_attib_prop_values:
         prop = ET.SubElement(header, 'prop', attrib={'type': 'x-Recognizers'})
         prop.text = 'RecognizeAll'
+
+    f_name_single_in = 'x-filename:SingleString' in header_attib_prop_values
+    f_name_single_out = 'x-filename:SingleString' not in header_attib_prop_values
+    f_name_multiple_in = 'x-filename:MultipleString' in header_attib_prop_values
+    f_name_multiple_out = 'x-filename:MultipleString' not in header_attib_prop_values
+    if f_name_single_in and f_name_multiple_out:
         prop = ET.SubElement(header, 'prop', attrib={'type': 'x-filename:MultipleString'})
-    # Modify the <tu> element by creating a sub-element
-    prop = ET.Element('prop', attrib={'type': 'x-filename:MultipleString'})
-    prop.text = prop_text
-    # Insert that element at the beginnig of another element
-    for tu in root.iter('tu'):
-        tu.insert(0, prop)
+        prop_to_remove = 'x-filename:SingleString'
+
+        for prop in header.iter('prop'):
+            if prop.get('type') == prop_to_remove:
+                header.remove(prop)
+
+        multi_el_prop = ET.Element('prop', attrib={'type': 'x-filename:MultipleString'})
+        multi_el_prop.text = prop_text
+
+        for tu in root.iter('tu'):
+            for prop in tu.iter('prop'):
+                if prop.get('type') == prop_to_remove:
+                    tu.remove(prop)
+            tu.insert(0, multi_el_prop)
+    elif f_name_single_out and f_name_multiple_out:
+        prop = ET.SubElement(header, 'prop', attrib={'type': 'x-filename:MultipleString'})
+        prop = ET.Element('prop', attrib={'type': 'x-filename:MultipleString'})
+        prop.text = prop_text
+        for tu in root.iter('tu'):
+            tu.insert(0, prop)
+    elif f_name_single_out and f_name_multiple_in:
+        # assume if filename is in header, it is also in <tu> 
+        for tu in root.iter('tu'):
+            for prop in tu.iter('prop'):
+                if prop.get('type') == 'x-filename:MultipleString':
+                    prop.text = prop_text
     indent(root)
     # return tree
     return ET.ElementTree(root)
@@ -110,7 +142,7 @@ def create_tmx(file1, file2):
     tmx_contents = get_tmx_file_contents(file1)
     _, prop_text = os.path.split(file1)
     tree = add_tree_elements(tmx_contents, prop_text)
-    # Save the modifed tmx file
+
     tree.write(file2, encoding='UTF-8', xml_declaration=True)
 
 
